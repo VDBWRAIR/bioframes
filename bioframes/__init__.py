44bfc3c5bb98904eb2933475ed8533525d532533
averagehat <michael.panciera.work@gmail.com>
2016-02-11 15:05:18 -0500

    more install stuff for pip

---
+++ bioframes/__init__.py
@@ -0,0 +1,6 @@
+__projectname__ = 'bioframes'
+__release__ = '0.0.1'
+__authors__ = "Michael Panciera"
+__authoremails__ = 'michael.panciera.work@gmail.com'
+__description__ = "Basic building blocks for bio-pipelines"
+__keywords__ = "pyjip, pipeline, bioinformatics"
